2ea28f94fce34040ddff05ec24e966c886500f48
Anand Gupta <anandg@asu.edu>
2023-03-27 02:47:28 -0700

    US-0 | Configure ui test coverage

--- sbs-metrics/ui/karma.conf.js
+++ sbs-metrics/ui/karma.conf.js
@@ -26,11 +26,12 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ui'),
+      dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'json-summary' }
       ]
     },
     reporters: ['progress', 'kjhtml'],
