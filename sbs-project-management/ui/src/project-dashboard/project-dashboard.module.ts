49745adaa61d0cbe904984338db382c31f0ecc8b
aanthir1 <aanthir1@asu.edu>
2023-03-28 22:47:15 -0700

    created screen for project ui

---
+++ sbs-project-management/ui/src/project-dashboard/project-dashboard.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TitlebarComponent } from './titlebar/titlebar.component';
+
+
+
+@NgModule({
+  declarations: [
+    TitlebarComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    TitlebarComponent
+  ]
+})
+export class ProjectDashboardModule { }
