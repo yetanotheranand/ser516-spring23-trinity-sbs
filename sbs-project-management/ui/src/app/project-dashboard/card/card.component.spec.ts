35dd9bf3c9c851f1dc3941a89f1787e1204fc85e
vinaythallam <98499360+vinaythallam@users.noreply.github.com>
2023-03-31 16:23:35 -0700

    US5Task14 - Added edit and delete buttons along with modifying their test cases
    Added edit and delete buttons along with modifying their test cases. Modified the folder structure of Projectdashboard by moving it inside app folder.

--- sbs-project-management/ui/src/app/project-dashboard/card/card.component.spec.ts
+++ sbs-project-management/ui/src/app/project-dashboard/card/card.component.spec.ts
@@ -20,4 +20,24 @@ describe('CardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should display the correct number of projects', () => {
+    const projectEls = fixture.nativeElement.querySelectorAll('.project');
+    expect(projectEls.length).toEqual(component.projects.length);
+  });
+
+  it('should display the correct project name and description', () => {
+    const projectEls = fixture.nativeElement.querySelectorAll('.project');
+    for (let i = 0; i < component.projects.length; i++) {
+      const projectName = projectEls[i].querySelector('a').textContent;
+      const projectDescription = projectEls[i].querySelector('div').textContent;
+      expect(projectName).toEqual(component.projects[i].name);
+      expect(projectDescription).toEqual(component.projects[i].description);
+    }
+  });
+
+  it('should display the "New Project" button', () => {
+    const buttonEl = fixture.nativeElement.querySelector('.titlebar button');
+    expect(buttonEl).toBeTruthy();
+    expect(buttonEl.textContent).toContain('New Project');
+  });
 });
--- sbs-project-management/ui/src/app/project-dashboard/titlebar/titlebar.component.spec.ts
+++ sbs-project-management/ui/src/app/project-dashboard/titlebar/titlebar.component.spec.ts
@@ -27,25 +27,4 @@ fdescribe('TitlebarComponent', () => {
     expect(titleEl.textContent).toContain('My Projects');
   });
 
-  it('should display the correct number of projects', () => {
-    const projectEls = fixture.nativeElement.querySelectorAll('.project');
-    expect(projectEls.length).toEqual(component.projects.length);
-  });
-
-  it('should display the correct project name and description', () => {
-    const projectEls = fixture.nativeElement.querySelectorAll('.project');
-    for (let i = 0; i < component.projects.length; i++) {
-      const projectName = projectEls[i].querySelector('a').textContent;
-      const projectDescription = projectEls[i].querySelector('div').textContent;
-      expect(projectName).toEqual(component.projects[i].name);
-      expect(projectDescription).toEqual(component.projects[i].description);
-    }
-  });
-
-  it('should display the "New Project" button', () => {
-    const buttonEl = fixture.nativeElement.querySelector('.titlebar button');
-    expect(buttonEl).toBeTruthy();
-    expect(buttonEl.textContent).toContain('New Project');
-  });
-
 });
