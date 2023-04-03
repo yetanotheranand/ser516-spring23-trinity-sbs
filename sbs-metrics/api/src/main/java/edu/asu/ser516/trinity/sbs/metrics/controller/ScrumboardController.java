d35fa88d64569b750ebd0f91a25460bb46e78596
manideepnalluri12 <mnallur2@asu.edu>
2023-04-02 20:42:00 -0700

    #36 added headers for cdf data updated

--- sbs-metrics/api/src/main/java/edu/asu/ser516/trinity/sbs/metrics/controller/ScrumboardController.java
+++ sbs-metrics/api/src/main/java/edu/asu/ser516/trinity/sbs/metrics/controller/ScrumboardController.java
@@ -14,14 +14,10 @@ import edu.asu.ser516.trinity.sbs.metrics.model.ScrumboardData;
 @RestController
 @RequestMapping("/scrumboard/v1")
 public class ScrumboardController {
-	
-
-
-	
 	@GetMapping("/data")
 	public List<ScrumboardData> getAllUsers() {
-
 		List<ScrumboardData> dataList = new ArrayList();
+
 		ScrumboardData data1 = new ScrumboardData();
 		data1.setDate("2023-03-02");
 		data1.setNew(5);
@@ -31,6 +27,41 @@ public class ScrumboardController {
 		data1.setBlocked(0);
 		dataList.add(data1);
 
+		ScrumboardData data2 = new ScrumboardData();
+		data2.setDate("2023-03-05");
+		data2.setNew(3);
+		data2.setInProgress(1);
+		data2.setReadyForTest(2);
+		data2.setClosed(2);
+		data2.setBlocked(0);
+		dataList.add(data2);
+
+		ScrumboardData data3 = new ScrumboardData();
+		data3.setDate("2023-03-07");
+		data3.setNew(4);
+		data3.setInProgress(0);
+		data3.setReadyForTest(1);
+		data3.setClosed(4);
+		data3.setBlocked(0);
+		dataList.add(data3);
+
+		ScrumboardData data4 = new ScrumboardData();
+		data4.setDate("2023-03-09");
+		data4.setNew(0);
+		data4.setInProgress(2);
+		data4.setReadyForTest(2);
+		data4.setClosed(1);
+		data4.setBlocked(0);
+		dataList.add(data4);
+
+		ScrumboardData data5 = new ScrumboardData();
+		data5.setDate("2023-03-11");
+		data5.setNew(0);
+		data5.setInProgress(0);
+		data5.setReadyForTest(0);
+		data5.setClosed(4);
+		data5.setBlocked(0);
+		dataList.add(data5);
 
 		return dataList;
 	}
