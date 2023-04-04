bf2f8f6464cd0d4fb3f79e797ecf54a12532b5a3
atulprakash18 <112651419+atulprakash18@users.noreply.github.com>
2023-04-04 15:31:52 -0700

    US-56 | TG-79 | Removed dependency error for Rest-Assured in sbs-driver

--- sbs-driver/api/src/test/java/edu/asu/ser516/trinity/sbs/driver/integration/DriverTest.java
+++ sbs-driver/api/src/test/java/edu/asu/ser516/trinity/sbs/driver/integration/DriverTest.java
@@ -5,7 +5,7 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DriverTest {
