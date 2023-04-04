455323c185ddf36a139038b34bc8543e054f622a
GokuS2k <112654934+GokuS2k@users.noreply.github.com>
2023-04-03 23:51:16 -0700

    api call to intergrate data with UI

---
+++ sbs-metrics/ui/src/app/get-api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class GetApiService {
+
+  constructor(
+    private http : HttpClient
+  ) { }
+  apicall(){
+    return this.http.get('http://localhost:8080/scrumboard/v1/data');
+  }
+}
