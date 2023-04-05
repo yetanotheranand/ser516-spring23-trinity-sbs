6a53df0c469bedba28a3871978ec2276124893a2
aanthir1 <98846515+aanthir1@users.noreply.github.com>
2023-04-05 14:58:22 -0700

    Update sbs-project-management/ui/src/project-dashboard/titlebar/titlebar.component.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- sbs-project-management/ui/src/project-dashboard/titlebar/titlebar.component.ts
+++ sbs-project-management/ui/src/project-dashboard/titlebar/titlebar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-titlebar',
   templateUrl: './titlebar.component.html',
-  styleUrls: ['./titlebar.component.css']
+  styleUrls: ['./titlebar.component.css'],
 })
 export class TitlebarComponent {
   projects = [
