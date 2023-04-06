6497692c1eb1b77decd97ef2d75061c1e36e9593
ljain3 <ljain3@asu.edu>
2023-04-05 21:20:37 -0700

    Created user interface to create a new user story.

--- sbs-project-management/ui/src/app/app-routing.module.ts
+++ sbs-project-management/ui/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserStoryComponent } from './user-story/user-story.component';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('../../src/project-dashboard/project-dashboard.module').then(m => m.ProjectDashboardModule) },
   { path: 'new-project', loadChildren: () => import('../../src/new-project/new-project.module').then(m => m.NewProjectModule) },
-
+  {path: 'user-story', component: UserStoryComponent}
 
 ];
 
--- sbs-project-management/ui/src/app/app.module.ts
+++ sbs-project-management/ui/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectDashboardModule } from './../project-dashboard/project-dashboard.module';
+import { UserStoryComponent } from './user-story/user-story.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserStoryComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     ProjectDashboardModule
   ],
