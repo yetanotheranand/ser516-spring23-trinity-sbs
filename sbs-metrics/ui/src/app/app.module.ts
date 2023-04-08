32c170bcffd2e566d558c7a9870cf51571456c0c
Drashti Chiragbhai Patel (Student) <dpate191@sundevils.asu.edu>
2023-04-07 20:55:29 -0700

    adding UI for graph

--- sbs-metrics/ui/src/app/app.module.ts
+++ sbs-metrics/ui/src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GraphsComponent } from './graphs/graphs.component';
-
+import { GraphComponent } from './graph/graph.component';
+import { NgChartsModule } from 'ng2-charts';
 @NgModule({
   declarations: [
     AppComponent,
-    GraphsComponent
+    GraphComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
