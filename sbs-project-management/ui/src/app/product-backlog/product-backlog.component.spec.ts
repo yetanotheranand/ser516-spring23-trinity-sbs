d7a4ce3a343cf36b0624083a88028cf3e13a5c77
ljain3 <ljain3@asu.edu>
2023-04-14 03:06:27 -0700

    Tests added product-backlog & user-story component

--- sbs-project-management/ui/src/app/product-backlog/product-backlog.component.spec.ts
+++ sbs-project-management/ui/src/app/product-backlog/product-backlog.component.spec.ts
@@ -1,16 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProductBacklogComponent } from './product-backlog.component';
+import { HttpClientModule } from '@angular/common/http';
+// import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProductBacklogService } from '../services/product-backlog.service';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
-describe('ProductBacklogComponent', () => {
+fdescribe('ProductBacklogComponent', () => {
   let component: ProductBacklogComponent;
   let fixture: ComponentFixture<ProductBacklogComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductBacklogComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientModule, ReactiveFormsModule],
+      declarations: [ProductBacklogComponent],
+      providers: [ProductBacklogService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProductBacklogComponent);
     component = fixture.componentInstance;
@@ -20,4 +26,25 @@ describe('ProductBacklogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the title of the page', () => {
+    const titleElement = fixture.debugElement.query(By.css('.titlebar h1')).nativeElement;
+    expect(titleElement.textContent).toEqual('Product Backlog');
+  });
+
+  it('should display all user stories correctly', () => {
+    const productBacklogData = [    
+        {      name: 'User Story 1',      status: 'New',    },    
+        {      name: 'User Story 2',      status: 'In progress',    },    
+        {      name: 'User Story 3',      status: 'Ready to Test',    }  
+    ];
+  
+    component.productBacklogData = productBacklogData;
+    fixture.detectChanges();
+  
+    const userStoryElements = fixture.nativeElement.querySelectorAll('.list-group-item');
+    expect(userStoryElements.length/2).toEqual(component.productBacklogData.length);
+
+    });
+
 });
--- sbs-project-management/ui/src/app/services/user-story.service.spec.ts
+++ sbs-project-management/ui/src/app/services/user-story.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UserStoryService } from './user-story.service';
 
-describe('UserStoryService', () => {
+fdescribe('UserStoryService', () => {
   let service: UserStoryService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserStoryService]
+    });
     service = TestBed.inject(UserStoryService);
   });
 
