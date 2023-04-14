46ba285cf985afaf8e3bce4bbea612c3adb2e18a
tejg22 <tguntupa@asu.edu>
2023-04-13 22:16:22 -0700

    Adding to fix Exlint issue

--- sbs-project-management/ui/src/app/project-dashboard/project/project.component.ts
+++ sbs-project-management/ui/src/app/project-dashboard/project/project.component.ts
@@ -9,20 +9,19 @@ import { ActivatedRoute } from '@angular/router';
 export class ProjectComponent implements OnInit {
   projects: any;
   id: string;
-  projectName: string = "";
-  projectDescription: string = "";
-  project: { id: string, name: string, description: string };
+  projectName = '';
+  projectDescription = '';
+  project: { id: string; name: string; description: string };
 
-  constructor(private route: ActivatedRoute) {
-
-  }
-
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
     this.projects = sessionStorage.getItem('Projects');
     if (this.projects) {
-      this.project = JSON.parse(this.projects).filter((e) => e.id == this.id)[0];
+      this.project = JSON.parse(this.projects).filter(
+        (e) => e.id == this.id
+      )[0];
       this.projectName = this.project.name;
       this.projectDescription = this.project.description;
     }
