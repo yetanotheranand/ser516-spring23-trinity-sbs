80dcca469e978419e1fc76f4b788a37c8afaaecf
tejg22 <tguntupa@asu.edu>
2023-04-15 13:21:06 -0700

    fixed space issue

--- sbs-project-management/ui/src/app/epic/new-epic/new-epic.component.ts
+++ sbs-project-management/ui/src/app/epic/new-epic/new-epic.component.ts
@@ -6,7 +6,7 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './new-epic.component.html',
   styleUrls: ['./new-epic.component.css'],
 })
-export class NewEpicComponent implements OnInit{
+export class NewEpicComponent implements OnInit {
   addEpicForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {}
