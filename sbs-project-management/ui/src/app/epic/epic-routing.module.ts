f6cc851befe7b434a5be7b0753d60c29ab2c8bc1
vinaythallam <98499360+vinaythallam@users.noreply.github.com>
2023-04-16 01:40:59 -0700

    Created flow between epics and new epic and projects
    Created flow between epics and new epic and projects

--- sbs-project-management/ui/src/app/epic/epic-routing.module.ts
+++ sbs-project-management/ui/src/app/epic/epic-routing.module.ts
@@ -4,19 +4,9 @@ import { NgModule } from '@angular/core';
 import { EpicBaseComponent } from './epic-base/epic-base.component';
 import { NewEpicComponent } from './new-epic/new-epic.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: EpicBaseComponent,
-    children: [
-      { path: 'list', component: EpicListComponent },
-      { path: 'new', component: NewEpicComponent },
-    ],
-  },
-];
+
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
 export class EpicRoutingModule {}
