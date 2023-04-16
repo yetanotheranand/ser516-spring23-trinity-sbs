dab61b920b293354fdddb3edb4d208d2f5e6d538
Akshat Bakliwal <abakliw1@asu.edu>
2023-04-15 20:48:38 -0700

    Dice Roll Layout

--- sbs-driver/ui/src/app/simulator/dice-roll/dice-roll.component.ts
+++ sbs-driver/ui/src/app/simulator/dice-roll/dice-roll.component.ts
@@ -1,8 +1,38 @@
 import { Component } from '@angular/core';
+import { map, Subject, switchMap, take, timer } from 'rxjs';
 
 @Component({
   selector: 'app-dice-roll',
   templateUrl: './dice-roll.component.html',
   styleUrls: ['./dice-roll.component.css'],
 })
-export class DiceRollComponent {}
+export class DiceRollComponent {
+  rolling = false;
+  selectedRollCount = 1;
+  selectedType = 4;
+  start$ = new Subject();
+
+  roller$ = this.start$
+    .pipe(
+      switchMap((max: any) => {
+        return timer(0, 50).pipe(take(10), map(_ => {
+          let total = 0;
+          for (let i = 0; i < this.selectedRollCount; i++) {
+            total += this.calcRoll(max);
+          }
+          return total
+        }))
+      })
+    )
+
+  calcRoll(max: number) {
+    const min = 1;
+    return Number((Math.random() * (max - min) + min).toFixed(0));
+  }
+
+  start(max: number) {
+    this.selectedType = max;
+    this.start$.next(max);
+  }
+}
+
