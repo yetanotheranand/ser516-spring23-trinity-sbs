6e1b80e126e70bf3051376fe2865d0f1b0cd5578
vinaythallam <98499360+vinaythallam@users.noreply.github.com>
2023-04-16 10:12:30 -0700

    Update project.component.spec.ts

--- sbs-project-management/ui/src/app/project-dashboard/project/project.component.spec.ts
+++ sbs-project-management/ui/src/app/project-dashboard/project/project.component.spec.ts
@@ -2,7 +2,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
 import { ActivatedRoute } from '@angular/router';
 import { ProjectComponent } from './project.component';
-import { of } from 'rxjs';
 import { RouterTestingModule } from '@angular/router/testing';
 import { RouterModule } from '@angular/router';
 fdescribe('ProjectComponent', () => {
