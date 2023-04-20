bfe39667298162750d8a871cbb555000d02de5b6
Aniket Ahir <ahiraniket@outlook.in>
2023-04-20 14:08:29 -0700

    dialog-box-tests-added-and-fixes

--- sbs-driver/ui/src/app/simulator/simulator.component.spec.ts
+++ sbs-driver/ui/src/app/simulator/simulator.component.spec.ts
@@ -1,22 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SimulatorComponent } from './simulator.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MaterialModule } from '../material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DialogBodyComponent } from './dialog-body/dialog-body.component';
 
 describe('SimulatorComponent', () => {
   let component: SimulatorComponent;
   let fixture: ComponentFixture<SimulatorComponent>;
+  let dialog: MatDialog;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SimulatorComponent],
+      imports: [MaterialModule, BrowserAnimationsModule],
+      declarations: [SimulatorComponent, DialogBodyComponent],
+      providers: [MatDialog],
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(SimulatorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    dialog = TestBed.inject(MatDialog);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open the dialog on button click', () => {
+    const spy = spyOn(dialog, 'open').and.callThrough();
+    component.openDialog();
+    expect(spy).toHaveBeenCalled();
   });
 });
