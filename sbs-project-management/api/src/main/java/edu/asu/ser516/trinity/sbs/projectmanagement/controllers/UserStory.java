989f68d213d7db2a1c275a1ebea0c9d8ce0c55ab
vinaythallam <98499360+vinaythallam@users.noreply.github.com>
2023-04-21 20:08:23 -0700

    Updated check style issues
    Updated check style issues

--- sbs-project-management/api/src/main/java/edu/asu/ser516/trinity/sbs/projectmanagement/controllers/UserStory.java
+++ sbs-project-management/api/src/main/java/edu/asu/ser516/trinity/sbs/projectmanagement/controllers/UserStory.java
@@ -1,6 +1,5 @@
 package edu.asu.ser516.trinity.sbs.projectmanagement.controllers;
 
-
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
@@ -12,20 +11,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 /**
- * The UserStory class is a REST controller that handles HTTP requests related to user stories in a project management tool.
- * It provides methods for retrieving all user stories for a specific project using the Taiga API.
+ * The UserStory class is a REST controller that handles HTTP requests.
+ * It provides methods for retrieving all user stories for a specific project using the
+ * Taiga API.
  */
 @RestController
 public class UserStory {
 
     private static String TAIGA_BASE_URL;
 
+    /**
+     * Sets the Taiga base URL from the application properties file.
+     *
+     * @param url the Taiga base URL
+     */
     @Value("${TAIGA_BASE_URL}")
     public void setTaigaBaseUrl(String url) {
         TAIGA_BASE_URL = url;
     }
 
+    /**
+     * Retrieves all user stories for a specific project using the Taiga API.
+     *
+     * @param projectId the ID of the project
+     * @param token     the authorization token for accessing the API
+     * @return a ResponseEntity containing the JSON response from the API
+     * @throws JSONException if there is an error parsing the JSON response
+     */
     @GetMapping("/userstories")
     public ResponseEntity<String> getAllUserStories(
             @RequestParam(name = "projectid", required = false) String projectId,
