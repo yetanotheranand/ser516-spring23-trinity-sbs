9a947013384135833ad53994f635a8f77bb5c12e
vinaythallam <98499360+vinaythallam@users.noreply.github.com>
2023-04-21 19:09:28 -0700

    Updated files to remove eslint errros
    Updated files to remove eslint errros

--- sbs-project-management/ui/src/app/product-backlog/product-backlog.component.ts
+++ sbs-project-management/ui/src/app/product-backlog/product-backlog.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {ProductBacklogService}from '../services/product-backlog.service';
-import { ActivatedRoute,ParamMap } from '@angular/router'
+import { ProductBacklogService } from '../services/product-backlog.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-product-backlog',
@@ -9,38 +9,41 @@ import { ActivatedRoute,ParamMap } from '@angular/router'
 })
 export class ProductBacklogComponent {
   // Replace list of user story data obtained from Taiga API in productBacklogData.
-  productBacklogData = [
-  ];
-  id:any;
-  constructor(private backlogService:ProductBacklogService,private route: ActivatedRoute){
+  productBacklogData = [];
+  id: any;
+  constructor(
+    private backlogService: ProductBacklogService,
+    private route: ActivatedRoute
+  ) {
     // this.getUserStories();
     this.route.queryParamMap.subscribe((params) => {
       this.id = params.get('projectid');
       console.log(this.id);
-      if(this.id!=null){
+      if (this.id != null) {
         this.getUserStories();
       }
     });
   }
-  getUserStories(){
+  getUserStories() {
     this.backlogService.listprojects(this.id).subscribe(
       (data) => {
-        console.log(data)
+        console.log(data);
         console.log(this.id);
-        this.productBacklogData = data
+        this.productBacklogData = data;
         console.log(this.productBacklogData);
         // this.router.navigateByUrl('/projects');
       },
       (error) => {
         if (error.status === 401) {
         }
+        this.productBacklogData = undefined;
       }
     );
   }
-  delete(id){
+  delete(id) {
     this.backlogService.deleteUserStory(this.id).subscribe(
       (data) => {
-        this.getUserStories()
+        this.getUserStories();
       },
       (error) => {
         if (error.status === 401) {
