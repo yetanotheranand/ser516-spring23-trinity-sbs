9f569ec65f48927bce59a1b4c810d767ced210a5
manideepnalluri12 <mnallur2@asu.edu>
2023-04-23 10:30:22 -0700

    #US40#44 Creating user interface connection

---
+++ sbs-metrics/api/src/main/java/edu/asu/ser516/trinity/sbs/metrics/ServiceImpl/ScrumboardService.java
@@ -0,0 +1,11 @@
+package edu.asu.ser516.trinity.sbs.metrics.service;
+
+import java.util.List;
+
+import edu.asu.ser516.trinity.sbs.metrics.model.ScrumboardData;
+
+public interface ScrumboardService {
+	
+	public List<ScrumboardData> getScrumboardDataList();
+
+}
--- sbs-metrics/api/src/main/java/edu/asu/ser516/trinity/sbs/metrics/controller/ScrumboardController.java
+++ sbs-metrics/api/src/main/java/edu/asu/ser516/trinity/sbs/metrics/controller/ScrumboardController.java
@@ -1,8 +1,12 @@
 package edu.asu.ser516.trinity.sbs.metrics.controller;
 
 import edu.asu.ser516.trinity.sbs.metrics.model.ScrumboardData;
+import edu.asu.ser516.trinity.sbs.metrics.service.ScrumboardService;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/scrumboard/v1")
 public class ScrumboardController {
+
+    @Autowired
+    ScrumboardService scrumboardService;
     /**
      * This method is used to GET metrics associated with the simulation run.
      *
@@ -20,19 +27,7 @@ public class ScrumboardController {
      */
     @GetMapping("/data")
     public List<ScrumboardData> getMetrics() {
-        List<ScrumboardData> dataList = new ArrayList();
-
-        ScrumboardData data1 = new ScrumboardData("2023-03-02", 5, 3, 2, 3, 0);
-        dataList.add(data1);
-        ScrumboardData data2 = new ScrumboardData("2023-03-05", 3, 1, 2, 2, 0);
-        dataList.add(data2);
-        ScrumboardData data3 = new ScrumboardData("2023-03-07", 4, 0, 1, 4, 0);
-        dataList.add(data3);
-        ScrumboardData data4 = new ScrumboardData("2023-03-09", 0, 2, 2, 1, 0);
-        dataList.add(data4);
-        ScrumboardData data5 = new ScrumboardData("2023-03-11", 0, 0, 0, 4, 0);
-        dataList.add(data5);
-
+        List<ScrumboardData> dataList = scrumboardService.getScrumboardDataList();
         return dataList;
     }
 }
