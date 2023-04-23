fc62868f5ae959752581cece495defe8d1a10c5e
Drashti Chiragbhai Patel (Student) <dpate191@sundevils.asu.edu>
2023-04-23 14:39:48 -0700

    added basic methods

--- sbs-metrics/api/src/main/java/edu/asu/ser516/trinity/sbs/metrics/model/MetricsData.java
+++ sbs-metrics/api/src/main/java/edu/asu/ser516/trinity/sbs/metrics/model/MetricsData.java
@@ -23,7 +23,67 @@ public class MetricsData {
         this.workVelocity = workVelocity;
     }
 
+    public int getStoryPoints() {
+        return storyPoints;
+    }
 
+    public void setStoryPoints(int storyPoints) {
+        this.storyPoints = storyPoints;
+    }
 
+    public int getStoriesCompleted() {
+        return storiesCompleted;
+    }
 
+    public void setStoriesCompleted(int storiesCompleted) {
+        this.storiesCompleted = storiesCompleted;
+    }
+
+    public int getBusinessValue() {
+        return businessValue;
+    }
+
+    public void setBusinessValue(int businessValue) {
+        this.businessValue = businessValue;
+    }
+
+    public int getFocusFactor() {
+        return focusFactor;
+    }
+
+    public void setFocusFactor(int focusFactor) {
+        this.focusFactor = focusFactor;
+    }
+
+    public int getWip() {
+        return wip;
+    }
+
+    public void setWip(int wip) {
+        this.wip = wip;
+    }
+
+    public int getStoriesTargeted() {
+        return storiesTargeted;
+    }
+
+    public void setStoriesTargeted(int storiesTargeted) {
+        this.storiesTargeted = storiesTargeted;
+    }
+
+    public int getWorkCapacity() {
+        return workCapacity;
+    }
+
+    public void setWorkCapacity(int workCapacity) {
+        this.workCapacity = workCapacity;
+    }
+
+    public int getWorkVelocity() {
+        return workVelocity;
+    }
+
+    public void setWorkVelocity(int workVelocity) {
+        this.workVelocity = workVelocity;
+    }
 }
