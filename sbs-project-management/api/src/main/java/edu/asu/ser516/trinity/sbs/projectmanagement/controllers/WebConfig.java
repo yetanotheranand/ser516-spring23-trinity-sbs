48bf522a140e5b65b74e140f6e38321e51744186
Anand Gupta <anandg@asu.edu>
2023-04-28 15:30:41 -0700

    m2_fix | Added CORS and networking patch

--- sbs-project-management/api/src/main/java/edu/asu/ser516/trinity/sbs/projectmanagement/controllers/WebConfig.java
+++ sbs-project-management/api/src/main/java/edu/asu/ser516/trinity/sbs/projectmanagement/controllers/WebConfig.java
@@ -1,15 +1,14 @@
 package edu.asu.ser516.trinity.sbs.projectmanagement.controllers;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig
-{
+public class WebConfig {
     @Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
--- sbs-project-management/ui/src/app/services/login.service.ts
+++ sbs-project-management/ui/src/app/services/login.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class LoginService {
-  url = 'http://localhost:8080/auth';
+  url = 'http://localhost:8080/auth/';
   constructor(private httpClient: HttpClient) {}
   performLogin(userDetails: any): Observable<any> {
     return this.httpClient.post(this.url, userDetails);
